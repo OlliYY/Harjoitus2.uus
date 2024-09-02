a93d6bc60388eacdfe8b6556443e375d297484ed
juhani.kuru <juhani.kuru@lapinamk.fi>
2024-09-02 15:12:23 +0300

    add example2

---
+++ controllers/users.py
@@ -0,0 +1,18 @@
+from flask import jsonify
+
+from models import User
+
+
+# Nyt jokaista controlleria vastaa yksi tiedosto. Tiedostot sisältävät kaikki funktiot,jotka pitävät
+# huolen requestin vastaanottamisesta ja responsen lähettämisestä.
+def get_all_users():
+    users =  User.get_all()
+    users_json = []
+    for user in users:
+        users_json.append({
+            'id': user.id,
+            'username': user.username,
+            'firstname': user.firstname,
+            'lastname': user.lastname,
+        })
+    return jsonify(users_json)
---
+++ models_postgres.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+
+class User:
+    def __init__(self, _id, username, firstname, lastname):
+        self.id = _id
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+
+    @classmethod
+    def get_all(cls):
+        with psycopg2.connect(user='root', database='sovelluskehykset_bad1') as con:
+            with con.cursor() as cur:
+                cur.execute('SELECT * FROM users')
+                result = cur.fetchall()
+                users = []
+                for user in result:
+                    users.append(cls(user[0], user[1], user[2], user[3]))
+
+                return users
